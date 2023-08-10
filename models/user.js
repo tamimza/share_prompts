df81171792b973ddf597d499d165e69c73c838ef
Tamim <kaytotamim@gmail.com>
2023-08-10 17:10:21 +0200

    fixed username length

--- models/user.js
+++ models/user.js
@@ -9,9 +9,10 @@ const userSchema = new Schema({
   username: {
     type: String,
     required: [true, "User name is required"],
+    // Adjusted regex for the username
     match: [
-      /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
-      "Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
+      /^(?![_])(?!.*[_]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+      "Username invalid! It shouldn't start or end with a dot or underscore.",
     ],
   },
   image: {
